f6a390747e6dc69f95156d252b8badeaa64b12b8
Guilherme Veloso <guilherme_veloso1@estudante.sesisenai.org.br>
2024-03-19 20:26:14 -0300

    exemplo

--- src/app.js
+++ src/app.js
@@ -3,14 +3,35 @@ const userRouter = require("./router/userRouter.js");
 const productRouter = require("./router/productRouter.js");
 const port = 3000;
 const app = express();
-app.get("/", (req, res) => {
-    res.send('<h1>hello world</h1>');
+app.get("/botao", (req, res) => {
+  res.send(`
+      <html>
+        <head>
+          <title>Exemplo de cliente servidor</title>
+          <script>
+            function redirecionarParaProduct() {
+              window.location.href = "/api/product";
+            }
+          </script>
+          <script>
+            function redirecionarParaUser() {
+              window.location.href = "/api/user";
+            }
+          </script>
+        </head>
+        <body>
+          <h1>Exemplo cliente servidor</h1>
+          <!-- Usando JavaScript para redirecionar para a página de destino -->
+          <button onclick="redirecionarParaProduct()">produto</button>
+          <button onclick="redirecionarParaUser()">usuario</button>
+        </body>
+      </html>
+    `);
 });
-app.use('/api', userRouter);
+app.use("/api", userRouter);
 
-app.use('/api', productRouter);
+app.use("/api", productRouter);
 
 app.listen(port, () => {
-    console.log("servidor online");
+  console.log("servidor online");
 });
-
